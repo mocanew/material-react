0d2fdcdbe2f68d0e371302af87627ed9207593cb
Mocanu Valentin <mr_vali@yahoo.ro>
2016-08-27 18:50:13 +0300

    Allow input to be changed by react state. The validator can now return a value to replace the one written by the user. Added onBlur function

--- src/Input/index.jsx
+++ src/Input/index.jsx
@@ -6,7 +6,7 @@ import _ from 'lodash';
 class Input extends React.Component {
     constructor(props) {
         super(props);
-        var state = _.merge({}, {
+        var defaults = {
             error: false,
             empty: true,
             name: '',
@@ -16,13 +16,15 @@ class Input extends React.Component {
             message: '',
             multiline: false,
             value: ''
-        }, props);
-        state.required = JSON.parse(props.required);
+        };
+        var state = _.merge({}, defaults, _.pick(props, Object.keys(defaults)));
         state.name = props.name ? props.name : props.children;
         state.title = props.title ? props.title : props.children;
         state.empty = state.value.length <= 0;
         this.state = state;
+        this.validator = _.isFunction(props.validator) ? props.validator : Input.defaultValidator;
         this.onInput = this.onInput.bind(this);
+        this.onBlur = this.onBlur.bind(this);
     }
     static get defaultProps() {
         return {
@@ -48,34 +50,37 @@ class Input extends React.Component {
             error: true
         }), 500);
     }
-    onInput(e) {
+    onBlur(e) {
+        this.onInput();
+        if (_.isFunction(this.props.onBlur)) this.props.onBlur(e);
+    }
+    onInput() {
         var input = this.refs.input.value;
-        if (typeof this.props.onInput == 'function') this.props.onInput(input);
 
-        if (typeof this.props.onChange == 'function' && this.state.value.trim() != input.trim()) this.props.onChange(input.trim());
+        if (_.isFunction(this.props.onInput)) this.props.onInput(input);
+        if (_.isFunction(this.props.onChange) && this.state.value.trim() != input.trim()) this.props.onChange(input.trim());
 
-        var validator = Input.defaultValidator(input, {
+        var validator = this.validator(input, {
             required: this.state.required
         });
-        if (typeof this.props.validator == 'function') {
-            validator = this.props.validator(input, {
-                required: this.state.required,
-                artificial: typeof e == 'boolean' ? e : false
-            });
+        if (!_.isString(validator.message)) {
+            validator.message = this.state.message;
         }
-        if (typeof validator.message != 'string') {
-            validator.message = this.props.message;
+        if (_.isString(validator.value)) {
+            input = validator.value;
         }
-        this.refs.input.style.height = 0;
-        var computedStyle = window.getComputedStyle(this.refs.input);
-        this.refs.input.style.height = (this.refs.input.scrollHeight - parseInt(computedStyle.paddingTop) - parseInt(computedStyle.paddingBottom)) + 'px';
+        if (this.state.multiline) {
+            this.refs.input.style.height = 0;
+            var computedStyle = window.getComputedStyle(this.refs.input);
+            this.refs.input.style.height = (this.refs.input.scrollHeight - parseInt(computedStyle.paddingTop) - parseInt(computedStyle.paddingBottom)) + 'px';
+        }
         this.setState({
             empty: validator.empty,
             error: validator.error,
             message: validator.message,
             value: input
         });
-        if (typeof validator.callback == 'function') {
+        if (_.isFunction(validator.callback)) {
             setTimeout(validator.callback);
         }
     }
@@ -87,7 +92,9 @@ class Input extends React.Component {
         });
     }
     componentDidMount() {
-        this.refs.input.style.height = 0;
+        if (this.state.multiline) {
+            this.refs.input.style.height = 0;
+        }
     }
     render() {
         var parentClasses = classNames({
@@ -97,10 +104,10 @@ class Input extends React.Component {
         });
         var field;
         if (this.state.multiline) {
-            field = <textarea className="input" name={this.state.name} onInput={this.onInput} onBlur={this.onInput} ref="input" defaultValue={this.state.value} />;
+            field = <textarea className="input" name={this.state.name} onInput={this.onInput} onBlur={this.onBlur} ref="input" value={this.state.value} />;
         }
         else {
-            field = <input className="input" type={this.state.type} name={this.state.name} onInput={this.onInput} onBlur={this.onInput} ref="input" required={this.state.required} defaultValue={this.state.value} />;
+            field = <input className="input" type={this.state.type} name={this.state.name} onInput={this.onInput} onBlur={this.onBlur} ref="input" required={this.state.required} value={this.state.value} />;
         }
         return (
             <div className={parentClasses} ref="parent">
@@ -115,17 +122,15 @@ class Input extends React.Component {
 }
 
 Input.propTypes = {
+    multiline: React.PropTypes.bool,
+    required: React.PropTypes.bool,
     name: React.PropTypes.string,
     children: React.PropTypes.string,
     title: React.PropTypes.string,
-    multiline: React.PropTypes.bool,
-    required: React.PropTypes.oneOfType([
-        React.PropTypes.bool,
-        React.PropTypes.string
-    ]),
     message: React.PropTypes.string,
     type: React.PropTypes.string,
     value: React.PropTypes.string,
+    onBlur: React.PropTypes.func,
     onChange: React.PropTypes.func,
     onInput: React.PropTypes.func,
     validator: React.PropTypes.func
