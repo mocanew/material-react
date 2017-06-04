d1a9e93eae7156f48f1ca69704b95d81b5e416fe
Valentin Mocanu <mrvali97@gmail.com>
2017-06-05 00:15:08 +0300

    Updated storybook

--- src/Button/story.js
+++ src/Button/story.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf } from '@storybook/react';
 import Button from './index.jsx';
 
 import '../_story.scss';
--- src/Card/story.js
+++ src/Card/story.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf } from '@storybook/react';
 import Card from './index.jsx';
 
 import '../_story.scss';
