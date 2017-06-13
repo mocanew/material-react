815f109aabc3aaa80309e96be4633c1f9a7c1dbe
Valentin Mocanu <mrvali97@gmail.com>
2017-06-13 18:10:06 +0300

    Added knobs for Button's story

--- src/Button/story.js
+++ src/Button/story.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf, action } from '@storybook/react';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs';
 import Button from './index.jsx';
 
 import '../_story.scss';
+
+var stories = storiesOf('Button', module);
+stories.addDecorator(withKnobs);
+
+stories.add('Knobs', () => {
+    var style = select('Style', {
+        flat: 'Flat',
+        raised: 'Raised',
+        none: 'None'
+    }, 'raised');
+
+    return (
+        <div>
+            <Button onClick={action('On click')} ripple={boolean('Ripple', true)} disabled={boolean('Disabled', false)} flat={style == 'flat'} raised={style == 'raised'}>Button</Button>
+        </div>
+    );
+});
+
+stories.add('Flat & raised', () => (
+    <div>
+        <Button flat>Flat button</Button>
+        <br /><br />
+        <Button raised>Raised button</Button>
+    </div>
+));
+
+stories.add('Big button (multi-touch test)', () => (
+    <div>
+        <Button raised className="bigButton">BIG button</Button>
+    </div>
+));
+
+stories.add('Ripple & no ripple', () => (
+    <div>
+        <Button raised ripple={false}>No ripple button</Button>
+        <br /><br />
+        <Button raised>Ripple button(default)</Button>
+    </div>
+));
 
 class StressTest extends React.Component {
     constructor() {
@@ -33,25 +73,4 @@ class StressTest extends React.Component {
         );
     }
 }
-
-storiesOf('Button', module)
-    .add('Flat & raised', () => (
-        <div>
-            <Button flat>Flat button</Button>
-            <br /><br />
-            <Button raised>Raised button</Button>
-        </div>
-    ))
-    .add('Big button (multi-touch test)', () => (
-        <div>
-            <Button raised className="bigButton">BIG button</Button>
-        </div>
-    ))
-    .add('Ripple & no ripple', () => (
-        <div>
-            <Button raised ripple={false}>No ripple button</Button>
-            <br /><br />
-            <Button raised>Ripple button(default)</Button>
-        </div>
-    ))
-    .add('Unmount cleanup test', () => <StressTest />);
+stories.add('Unmount cleanup test', () => <StressTest />);
